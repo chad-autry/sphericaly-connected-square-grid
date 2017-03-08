d75189aac42eacf53595cae4af82de7925c38b82
Chad Autry <chad.autry@gmail.com>
2017-03-08 07:28:05 -0600

    Fix undefined variable

--- src/components/MapData.js
+++ src/components/MapData.js
@@ -4,20 +4,20 @@ module.exports = function MapData(rows, columns) {
     this.cells = [];
 
     for (var i = 0; i < rows; i++) {
-        cells[i] = [];
+        this.cells[i] = [];
         for (var j = 0; j < columns; j++) {
             if (Math.floor(rows/2) === i && Math.floor(columns/2) === j) {
-                cells[i][j] = "fa fa-at";
+                this.cells[i][j] = "fa fa-at";
             } else if (Math.random() < .1) {
-                cells[i][j] = "fa fa-tree";
+                this.cells[i][j] = "fa fa-tree";
             } else {
-                cells[i][j] = "";
+                this.cells[i][j] = "";
             }
         }
     }
 
    this.getEntity = function(row, column) {
-       return cells[row][column];
+       return this.cells[row][column];
    };
 
 };
