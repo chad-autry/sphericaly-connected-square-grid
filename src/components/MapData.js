b20369ce36a4ff99f7e1629a646422d35bfa3ea6
Chad Autry <chad.autry@gmail.com>
2017-03-11 14:28:20 -0600

    Spherical Follow

--- src/components/MapData.js
+++ src/components/MapData.js
@@ -28,21 +28,59 @@ module.exports = function MapData(rows, columns) {
 
    this.getEntity = function(row, column) {
        let mappedRow = row;
+       let mappedColumn = column;
        let faFlip = "";
+       
+       //Flip the whole map for Spherical Flip
        if (this.wrapStyle === "Spherical Flip" && this.flipped) {
            faFlip = " fa-flip-vertical";
            mappedRow = -1*(row - this.rows + 1);
        }
+       
+       //For Spherical Follow, re-center the whole view to be relative around the @ character
+       if (this.wrapStyle === "Spherical Follow") {
+           //Figure relative column
+           mappedColumn = (column + (this.atColumn - (this.columns / 2)) + this.columns) % this.columns;
+           
+           //If flipped, mirror the requested row
+	   if (this.flipped) {
+	       mappedRow = -1*(mappedRow - this.rows + 1);
+           }
+           
+           //Then shift the row to be relative to the character
+           mappedRow = mappedRow + (this.atRow - (this.rows / 2));
+           
 
-       if (mappedRow === this.atRow && column === this.atColumn) {
+           //Then finally wrap if it is looking across a pole
+           if (mappedRow < 0) {
+               mappedRow = -1*(mappedRow + 1);
+               mappedColumn = (mappedColumn + this.columns/2) % this.columns;
+               //We're looking over a pole, and the view needs to be flipped relative to next to the character
+               if (!this.flipped) {
+                   faFlip = " fa-flip-vertical";
+               }
+           } else if (mappedRow >= this.rows) {
+               mappedRow = this.rows - (1 + (mappedRow % this.rows));
+               mappedColumn = (mappedColumn + this.columns/2) % this.columns;
+               //We're looking over a pole, and the view needs to be flipped relative to next to the character
+               if (!this.flipped) {
+                   faFlip = " fa-flip-vertical";
+               }
+           } else if (this.flipped) {
+               //We aren't looking over a pole, but we've traveled over one and our local view is flipped
+               faFlip = " fa-flip-vertical";
+           }
+       }
+
+       if (mappedRow === this.atRow && mappedColumn === this.atColumn) {
            return "fa fa-at";
        } else {
-           return this.cells[mappedRow][column] + faFlip;
+           return this.cells[mappedRow][mappedColumn] + faFlip;
        }
    };
 
    this.up = function(flipOverride) {
-       if (!flipOverride && this.wrapStyle === "Spherical Flip" && this.flipped) {
+       if (!flipOverride && (this.wrapStyle === "Spherical Follow" || this.wrapStyle === "Spherical Flip") && this.flipped) {
            return this.down(true);
        }
 
@@ -89,7 +127,7 @@ module.exports = function MapData(rows, columns) {
    };
 
    this.down = function(flipOverride) {
-       if (!flipOverride && this.wrapStyle === "Spherical Flip" && this.flipped) {
+       if (!flipOverride && (this.wrapStyle === "Spherical Follow" || this.wrapStyle === "Spherical Flip") && this.flipped) {
            return this.up(true);
        }
        let nextRow = this.atRow;
